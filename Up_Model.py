33e411f97292e4904e7017cf142e321c11e80a63
Kostya <110357593+KD2D@users.noreply.github.com>
2025-06-11 20:02:24 +0300

    Обновление не знаю будет ли оно добавлен requirements.txt

--- Up_Model.py
+++ Up_Model.py
@@ -1,26 +1,40 @@
 # Этот модуль выполняет все главные штуки
-# Например вывод текста или обновление кода во время работы программы
+# Например вывод текста или обновление кода во время работы программы.
 
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from peft import PeftModel, PeftConfig
-import torch
 import module
+import gc
+import torch
 
+
+gc.collect()
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+    torch.cuda.ipc_collect()  # иногда помогает при залипании кеша
+    torch.cuda.synchronize()  # синхронизация операций
 
 model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 cache_path = "D:/AIR" # Поменяйте на путь к вашему репозиторию с моделью
 fine_tuned_path = "D:/AIR/fine-training"
 
 
-# Загрузка модели
+# Конфигурация 4-bit квантизации
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",  # или "fp4" — nf4 чуть лучше
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.float16
+)
+
+# Загрузка модели с 4-bit конфигом
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     cache_dir=cache_path,
-    device_map="cuda",
-    torch_dtype=torch.float16,
-    load_in_4bit=True)
-
+    quantization_config=bnb_config,
+    device_map="auto" # Автоматически на GPU
+)
 # Загрузка токенизатора
 tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir=cache_path)
 
@@ -28,52 +42,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir=cache_path)
 # model_training = PeftModel.from_pretrained(model, fine_tuned_path)
 
 # перевести в eval режим (ускоряет и снижает память или нет)
-# model.eval()
+model.eval()
 
-
-
-module.Start(model, tokenizer)
-
-
-# Старый код можно удалить потом
-# # Функция общения
-# def chat(prompt, history=None, max_new_tokens=256):
-#     #Что мы вводим
-#     system_prompt = "You are a helpful, respectful, and honest assistant.\n"
-#
-#     full_prompt = f"<|system|>\n{system_prompt}</s>\n"
-#
-#     if history:
-#         for user, assistant in history:
-#             full_prompt += f"<|user|>\n{user}</s>\n<|assistant|>\n{assistant}</s>\n"
-#
-#     full_prompt += f"<|user|>\n{prompt}</s>\n<|assistant|>\n"
-#
-#     # Оброботка и вывод
-#     inputs = tokenizer(full_prompt, return_tensors="pt").to("cuda")
-#     outputs = model.generate(
-#         **inputs,
-#         max_new_tokens=max_new_tokens,
-#         do_sample=True,
-#         top_p=0.95,
-#         temperature=0.7,
-#         repetition_penalty=1.1
-#     )
-#     decoded = tokenizer.decode(outputs[0], skip_special_tokens=False)
-#
-#     # Сокращение вывода нейросети и отправка. Получим только (последний) ответ
-#     answer = decoded.split("<|assistant|>\n")[-1].split("</s>")[0].strip()
-#
-#     return answer
-#
-#
-# history = []
-#
-# while True:
-#     user_input = input("Ты: ")
-#     if user_input.lower() in ["exit", "quit", "выход"]:
-#         break
-#
-#     response = chat(user_input, history)
-#     print("ИИ:", response)
-#     history.append((user_input, response))
+# Запускаем дельше остальную работу
+module.start(model, tokenizer)
