bc14081ae8762604d5ca2cc6532a077dfc11c5a6
Kostya <110357593+KD2D@users.noreply.github.com>
2025-06-11 19:24:03 +0300

    Всё сломалось переделываю репозиторий на гит, код обновлёт

---
+++ README.md
@@ -0,0 +1,58 @@
+# Console-Chat-for-Meta-Llama-Inst
+
+Это код для работы с GPT моделью Meta-Llama-B-Instruct в консоле.
+
+Для корректной работы кода обязательно установите модель по этой ссылке: `https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct`
+
+Этот код создан в учебных целях и продолжает развиваться.  
+Вы можете свободно использовать его, изменять, предлагать улучшения и идеи.
+> 🤍 Если код оказался полезен — дайте знать. Это мотивирует развивать дальше!
+
+Хорошо работает на RTX 2060 Super (8 ГБ VRAM)
+
+## Как пользоватся этим репозиторием:
+
+⚠ Python версия 3.9+
+
+Cкачайте репозиторий удобным вам способом в Консоли или PowerShell:
+`git clone https://github.com/KD2D/Chat-for-Meta-Llama-Inst.git`
+
+Установка PyTorch с CUDA 12.1:
+`pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121`
+
+Установите остальные зависимости:
+`pip install -r requirements.txt`
+
+Запустите `Up_Model.py`
+
+Если проект получит популярность, я переведу все комментарии в коде на русский и английский языки.
+<br><br>
+
+---
+
+This is the code for working with the Meta-Llama-B-Instruct GPT model in the console.
+
+To ensure proper operation of the code, make sure to install the model from this link: `https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct`
+
+This code was created for educational purposes and is under active development.  
+You are free to use it, modify it, and suggest improvements and ideas.  
+> 🤍 If the code was useful — let me know. That motivates further development!
+
+Works well on RTX 2060 Super (8 GB VRAM)
+
+## How to use this repository:
+
+⚠ Python version 3.9+
+
+Download the repository in any convenient way using Console or PowerShell:
+`git clone https://github.com/KD2D/Chat-for-Meta-Llama-Inst.git`
+
+Install PyTorch with CUDA 12.1:
+`pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121`
+
+Install the remaining dependencies:
+`pip install -r requirements.txt`
+
+Run `Up_Model.py`
+
+If the project becomes popular, I will translate all comments in the code into both Russian and English.
--- module.py
+++ module.py
@@ -2,27 +2,23 @@
 # Например вывод текста или обновление кода во время работы программы
 # When this code is useful to someone, I will probably translate it into English
 
+import re
 import torch
+import traceback
+import json
 import importlib
 import sys
-from transformers import TextStreamer
-
-# Класс мы добавили класс для стриминга
-class SavingTextStreamer(TextStreamer):
-    def __init__(self, tokenizer):
-        super().__init__(tokenizer, skip_prompt=True, skip_special_tokens=True)
-        self.generated_text = ""  # сюда мы копим весь текст
-
-    def on_text(self, text: str, **kwargs):
-        print(text, end="", flush=True)  # печатаем сразу
-        self.generated_text += text       # и параллельно копим текст
-
+from threading import Thread
+from transformers import TextIteratorStreamer
 
 
 # Функция общения со стримингом
-def chat_stream(model, tokenizer, prompt="", history=None, max_new_tokens=512, system_prompt=""):
-    try:
+def chat_stream(model, tokenizer, prompt="", history=None, max_new_tokens=512, system_prompt=""): # Проблема с непрерывным выводом от нейросети
 
+    """Эта функция выполняет последовательный вывод текста с помощью
+    TextIteratorStreamer в transformers перед этим добавляя специальные символы для модели"""
+
+    try:
         #Тут нам нужен system_prompt
         full_prompt = f"<|system|>\n{system_prompt}</s>\n"
 
@@ -32,45 +28,54 @@ def chat_stream(model, tokenizer, prompt="", history=None, max_new_tokens=512, s
 
         full_prompt += f"<|user|>\n{prompt}</s>\n<|assistant|>\n"
 
-        # Оброботка и вывод
+        # Обработка и вывод
         inputs = tokenizer(full_prompt, return_tensors="pt").to("cuda")
-
         print("ИИ: ", end="")
 
-        streamer = SavingTextStreamer(tokenizer) # отсылка на класс
+        stop_token_ids = tokenizer.convert_tokens_to_ids(["<|system|>", "<|user|>", "</s>", "<|assistant|>"]) # Добавь сюда <|eot_id|> если хочешь посмотреть на баги :)
 
-        #with torch.no_grad():  # Генерация ответа без отслеживания градиентов (Tab)
-        outputs = model.generate( # ТЫ стример Гарри (Зачем тут outputs?)
-            **inputs,
-            max_new_tokens=max_new_tokens,
-            do_sample=True,
-            top_p=0.90, # Выбирает токины у которых вероятность записана тут
-            temperature=0.7, # Чем выше тем ответы будут хаотичнее
-            repetition_penalty=1.1,
-            # pad_token_id=tokenizer.eos_token_id, Добавь что бы сломать GPT
-            # skip_special_tokens=True # На случай если мне нужны спец-токены <|system|>, <|user|>, <|assistant|>
-            streamer=streamer
-        )
+        streamer = TextIteratorStreamer(tokenizer) # отсылка на класс
+
+        generation_kwargs = dict(inputs, streamer=streamer, max_new_tokens=max_new_tokens)
+        thread = Thread(target=model.generate, kwargs=generation_kwargs)
+        thread.start()
+        thread.join()
+        generated_text = ""
+        for new_text in streamer:
+            print(new_text, end="", flush=True)
+            generated_text += new_text
+
+        # answer = generated_text.strip()
         # После генерации:
-        answer = streamer.generated_text.strip()
+        print()
+
+        # Удалить сдужебный токен
+
+        # Сокращение вывода нейросети и отправка. Получим только (последний) ответ
+
+        # match = re.search(r"<\|assistant\|\>\n(.*?)</s>", generated_text, re.DOTALL)
+        answer = generated_text.split("<|assistant|>\n")[-1].split("</s>")[0].strip()
 
         # Очистка неиспользуемой памяти
         torch.cuda.empty_cache()
+        del generated_text
         del inputs
-        del outputs
-        print("Вывод стримера", answer)
+        #del outputs
+        # print("Вывод стримера", answer) # это проверка. Уже исправленно
         return answer
 
     except Exception as e:
         print(f"Ошибка чата 1 {e}")
+        handle_error(e)
 
 
 
 # Функция общения нормальная
 def chat_normal(model, tokenizer, prompt="", history=None, max_new_tokens=512, system_prompt=""):
-    try:
 
-        #Промт дня "Ты говоришь как пират, с "Arrr!" в каждом предложении"
+    """Эта функция выполняет полный вывод текста перед этим добавляя специальные символы для модели"""
+
+    try:
 
         #Что мы вводим в system_prompt будет тут
         full_prompt = f"<|system|>\n{system_prompt}</s>\n"
@@ -113,53 +118,102 @@ def chat_normal(model, tokenizer, prompt="", history=None, max_new_tokens=512, s
 
     except Exception as e:
         print(f"Ошибка чата 0 {e}")
+        handle_error(e)
 
 
-# Перезагружаем не забудь нажать ctrl + s
+def handle_error(e): # Просто выводит полную ошибку
+    choice = input("Вывести полный traceback? (y/n): ")
+    if choice.lower() == 'y':
+        traceback.print_exc()
+
+
+
+# Перезагружаем не забудь нажать ctrl + s заранее
 def RestartKey(model, tokenizer, history):
     try:
-        # Модуль_re = sys.modules[__name__] # Вроде бы не ннада
+        # Модуль_re = sys.modules[__name__] # Вроде бы не нннада
 
         print("Перезапуск!")
         importlib.reload(sys.modules[__name__])  # <-- файл (модуль.py) перезагружает себя
-        return Start(model, tokenizer, history)# Востанавливаем ход
+        return start(model, tokenizer, history)# Восстанавливаем ход
     except Exception as e:
         print(f"Ошибка перезагрузки {e}")
+        handle_error(e)
 
 
 
-def Start(model, tokenizer, history=None): # Запускаем
+def save_in_file(file_path, text, response): # Сохраняет
     try:
+        with open(file_path, "a", encoding="utf-8") as file:
+            data = [text, response]
+            json.dump(data, file, ensure_ascii=False, indent=2)
+            file.write("\n")  # перенос строки
+
+    except Exception as e:
+        print(f"Ошибка сохранения файла {e}")
+        handle_error(e)
+
+
+def read_file(file_path): #Читает
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            return json.load(file)
+
+    except Exception as e:
+        print(f"Ошибка загрузки файла {e}")
+        handle_error(e)
+        return ""
+
+
+def start(model, tokenizer, history=None):  # Запускаем
+    """
+    Стартуем!
+
+    >>> (CTRL + S) потом напиши re для перезагрузки кода без перезагрузки модели
+
+    >>> exit/выход
+
+    >>> del/стереть отчищает память
+    """
+    try:
+        # Промт дня "Ты говоришь как пират, с "Arrr!" в каждом предложении"
+
         # Это будет вводится как основа для модели
-        system_prompt = """Ты — Тим, добрый ИИ помошник.\n"""
+        system_prompt = """Ты — Тим, добрый ИИ помошник. Луна - это не сыр\n"""
 
-
-        stream = input("Стриминг, нет? 1 или 0 (0 defolt): ")
+        file = "memory.json"
+        question = input("Стриминг, нет? 1 или 0 (0 defolt): ")
 
         if history is None: # история есть?
             history = []
+            # history.append(read_file(file))
 
         while True:
             print(f"Ранее: {history}")  # можно потом убрать
-
             user_input = input("Ты: ") # Ввод пользователя
+
             if user_input.lower() in ["re", "restart", "reload"]: # Перезагружаем модуль? Не?
                 return RestartKey(model, tokenizer, history)
                 # continue # Это пока побудет тут
-
-            elif user_input.lower() in ["exit", "quit", "выход", "покинуть"]: # Alt + F4 (как это этот код)
+            elif user_input.lower() in ["exit", "quit", "выход", "покинуть"]: # Alt + F4 и Ctrl + c(как это этот код)
                 break
+            elif user_input.lower() in ["стереть", "del"]: # Делай только в крайнем случае
+                # model.reset_cache()  # зависит от еслинужно отчищать всё
+                history.clear()
+                continue
 
             else: # Ну значит это нейронке + узнаём какой метод общения используем
-
-                if (stream == "1"):
+                if (question == "1"):
                     response = chat_stream(model, tokenizer, prompt=user_input, history=history, system_prompt=system_prompt)
                     # -
                 else:
                     response = chat_normal(model, tokenizer, prompt=user_input, history=history, system_prompt=system_prompt)
                     print("ИИ:", response)
                 history.append((user_input, response))
+                save_in_file(file, user_input, response)
+
 
     except Exception as e:
-        print(f"Ошибка главного цикал 0 {e}")
-
+        print(f"Ошибка главного цикла 0 {e}")
+        handle_error(e)
+        RestartKey(model, tokenizer, history)
